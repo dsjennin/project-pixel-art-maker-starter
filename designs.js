d2b06a49022b37d7250bb89ba249e23081d4b0e2
dsjennin <dave.s.jennings@gmail.com>
2022-05-18 16:29:29 -0600

    grid working

--- designs.js
+++ designs.js
@@ -1,13 +1,39 @@
 // Select color input
 // Select size input
 
+let inputHeight = document.getElementById('inputHeight')
+let inputWidth = document.getElementById('inputWidth')
+let colorPicker = document.getElementById('colorPicker')
+let pixelCanvas = document.getElementById('pixelCanvas')
+let sizePickerForm = document.getElementById('sizePicker');
+
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+sizePickerForm.addEventListener('submit', (e) => {
+     e.preventDefault(); 
+    if (pixelCanvas.firstElementChild != null) pixelCanvas.firstElementChild.remove();
+    makeGrid(inputHeight.value, inputWidth.value);
+});
 
-alert("called makeGrid");
+
+
+function makeGrid(height, width) {
+for(i = 0; i < height; i++) {
+    // insert row using insertRow function, this will give you a reference of the created row
+    let row = pixelCanvas.insertRow(i)
+    for(j = 0; j < width; j++) {
+       // insert column using insertCell function into the created row
+       let cell = row.insertCell(j)
+
+       cell.addEventListener('click', (e) => {
+       cell.style.backgroundColor = colorPicker.value;
+    });
+
+
+    } 
+  }
+
 
 
 }
